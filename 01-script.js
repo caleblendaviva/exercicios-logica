61fa5b3541025c71c2443a1d5f794e1118f218cf
caleblendaviva <indrababr@gmail.com>
2025-05-28 17:08:12 -0300

    atividades dia 28/5

---
+++ 01-script.js
@@ -0,0 +1,9 @@
+//SQ2 ex9 - mensagem secreta
+let mensagem
+
+senha = prompt("qual palavrinha magica?")
+if(senha == "abracadabra"){
+    alert("mensagem secreta")
+}else{
+    alert("ta errado o animal")
+}
---
+++ 02-script.js
@@ -0,0 +1,10 @@
+//sg2, ex 13 - verificando votaçao
+let idade
+idade = prompt ("voce esta aqui para votar, qual sua idade?")
+if(idade >= 16){
+    alert("voce pode votar")
+}
+if(idade < 16){
+    alert("voce ainda nao pode votar")
+}
+   
